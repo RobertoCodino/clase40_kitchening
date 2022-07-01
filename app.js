63112d7ceaa22d8c80a95690716d220a78f3f0fb
RobertoCodino <codinodeian2008@gmail.com>
2022-07-01 16:47:20 -0300

    estructura

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const path = require('path')
-const port = 3030
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3030;
 
-const app = express
+app.use(express.static('public'));
 
-app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
 
-
-app.listen
+app.listen(port , () => console.log('Server running in http://localhost:' + port))
